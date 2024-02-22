221f232f28d9adebf39d4467f6697dc535120772
Alex <al.rusev@gmail.com>
2024-02-22 13:45:43 +0200

    Created Travel model and DB links

--- src/main/java/com/telerikacademy/web/carpooling/models/Travel.java
+++ src/main/java/com/telerikacademy/web/carpooling/models/Travel.java
@@ -1,6 +1,7 @@
 package com.telerikacademy.web.carpooling.models;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "travels")
@@ -14,11 +15,78 @@ public class Travel {
     @JoinColumn(name = "organizer_id")
     private User driver;
 
+    @Column(name = "start_point", nullable = false, length = 255)
+    private String startPoint;
+
+    @Column(name = "end_point", nullable = false, length = 255)
+    private String endPoint;
+
+    @Column(name = "departure_time", nullable = false)
+    private LocalDateTime departureTime;
+
+    @Column(name = "free_spots", nullable = false)
+    private int freeSpots;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_status")
+    private Status status;
+
+    public Travel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public User getDriver() {
         return driver;
     }
 
     public void setDriver(User driver) {
         this.driver = driver;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getFreeSpots() {
+        return freeSpots;
+    }
+
+    public void setFreeSpots(int freeSpots) {
+        this.freeSpots = freeSpots;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
